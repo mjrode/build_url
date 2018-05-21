c684b1d3a72cfd759a918afc5e886330848bf580
mike.rode <mike.rode@malauzai.com>
2018-05-20 19:38:49 -0500

    Add option to pass in custom email if sending build to someone else.

--- lib/build_url.rb
+++ lib/build_url.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 require 'mail'
 require 'yaml'
+
 require_relative "../lib/build_url/compose"
 require_relative "../lib/build_url/mailer"
 
 def run
   config = get_config
   return unless valid_email?(config['email'])
+  get_email
   prompt_user
   params = build_params(gets.chomp, config)
   Mailer.mail(params)
@@ -30,12 +32,22 @@ def valid_email?(email)
   valid_email
 end
 
+def get_email
+  puts 'Do you want to send this to someone else (Yes/No, Y/N, y/n)'
+  response = gets.chomp
+  if response.downcase == 'y' || 'yes' 
+    puts 'Please enter the email address'
+    @email = gets.chomp
+  end
+end
+
 def prompt_user
   puts "\nPlease enter the second to last line of the build\n\n"
   puts "Example: `13:23:47 Publish artifacts to S3 Bucket bucket=mlzAndroid, file=084-5.3.1665-preproduction.apk`"
 end
 
 def build_params(build_details, config)
+  config['email'] = @email if @email
   url = Compose.build_url(build_details, config)
   subject = Compose.build_subject(build_details)
   params = {url: url, subject: subject, config: config}
